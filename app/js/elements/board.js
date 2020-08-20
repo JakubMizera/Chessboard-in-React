2621fc32cca63e95f81e224917834e90d9c98900
JakubMizera <kubamiz53@gmail.com>
2020-08-20 12:26:40 +0200

    position on board

--- app/js/elements/board.js
+++ app/js/elements/board.js
@@ -3,100 +3,121 @@ import {useEffect, useState} from 'react';
 
 
 const Board = () => {
-    const [position, setPosition] = useState([]);
+    const positionArray = [];
+    const boardSize = 8;
 
-    const handlePosition = () => {
-        const pawnWhite = <img src="../../images/pawn-white.svg"/>
+    for (let i = 0; i < boardSize; i++) {
+        positionArray[i] = [];
+
+        for (let j = 0; j < boardSize; j++) {
+            positionArray[i][j] = {row: i, col: j};
+        }
+    }
+
+    positionArray[0][0].piece = "rook";
+    positionArray[0][1].piece = "knight";
+    positionArray[0][1].player= "black";
+
+    const refreshBoard = () => {
 
     }
+
+    const handleMove = (e) => {
+        let dataset = e.currentTarget.dataset;
+        console.log(positionArray[dataset.row][dataset.col]);
+        refreshBoard();
+    }
+
+    const kingBlack = <img src='../../images/king-black.svg'/>
+
 
     return (
         <>
             <div className='board'>
                 <div className='row'>
-                    <div className='col white' id='a8'>8</div>
-                    <div className='col black' id='b8'></div>
-                    <div className='col white' id='c8'></div>
-                    <div className='col black' id='d8'></div>
-                    <div className='col white' id='e8'></div>
-                    <div className='col black' id='f8'></div>
-                    <div className='col white' id='g8'></div>
-                    <div className='col black' id='h8'></div>
+                    <div className='col white' data-row='0' data-col='0' onClick={handleMove.bind(this)}>8</div>
+                    <div className='col black' data-row='0' data-col='1' onClick={handleMove.bind(this)}></div>
+                    <div className='col white' data-row='0' data-col='2' onClick={handleMove.bind(this)}></div>
+                    <div className='col black' data-row='0' data-col='3' onClick={handleMove.bind(this)}></div>
+                    <div className='col white' data-row='0' data-col='4' onClick={handleMove.bind(this)}>{kingBlack}</div>
+                    <div className='col black' data-row='0' data-col='5' onClick={handleMove.bind(this)}></div>
+                    <div className='col white' data-row='0' data-col='6' onClick={handleMove.bind(this)}></div>
+                    <div className='col black' data-row='0' data-col='7' onClick={handleMove.bind(this)}></div>
                 </div>
                 <div className='row'>
-                    <div className='col black' id='a7'>7</div>
-                    <div className='col white' id='b7'></div>
-                    <div className='col black' id='c7'></div>
-                    <div className='col white' id='d7'></div>
-                    <div className='col black' id='e7'></div>
-                    <div className='col white' id='f7'></div>
-                    <div className='col black' id='g7'></div>
-                    <div className='col white' id='h7'></div>
+                    <div className='col black' data-row='1' data-col='0' onClick={handleMove.bind(this)}>7</div>
+                    <div className='col white' data-row='1' data-col='1' onClick={handleMove.bind(this)}></div>
+                    <div className='col black' data-row='1' data-col='2' onClick={handleMove.bind(this)}></div>
+                    <div className='col white' data-row='1' data-col='3' onClick={handleMove.bind(this)}></div>
+                    <div className='col black' data-row='1' data-col='4' onClick={handleMove.bind(this)}></div>
+                    <div className='col white' data-row='1' data-col='5' onClick={handleMove.bind(this)}></div>
+                    <div className='col black' data-row='1' data-col='6' onClick={handleMove.bind(this)}></div>
+                    <div className='col white' data-row='1' data-col='7' onClick={handleMove.bind(this)}></div>
                 </div>
                 <div className='row'>
-                    <div className='col white' id='a6'>6</div>
-                    <div className='col black' id='b6'></div>
-                    <div className='col white' id='c6'></div>
-                    <div className='col black' id='d6'></div>
-                    <div className='col white' id='e6'></div>
-                    <div className='col black' id='f6'></div>
-                    <div className='col white' id='g6'></div>
-                    <div className='col black' id='h6'></div>
+                    <div className='col white' data-row='2' data-col='0' onClick={handleMove.bind(this)}>6</div>
+                    <div className='col black' data-row='2' data-col='1' onClick={handleMove.bind(this)}></div>
+                    <div className='col white' data-row='2' data-col='2' onClick={handleMove.bind(this)}></div>
+                    <div className='col black' data-row='2' data-col='3' onClick={handleMove.bind(this)}></div>
+                    <div className='col white' data-row='2' data-col='4' onClick={handleMove.bind(this)}></div>
+                    <div className='col black' data-row='2' data-col='5' onClick={handleMove.bind(this)}></div>
+                    <div className='col white' data-row='2' data-col='6' onClick={handleMove.bind(this)}></div>
+                    <div className='col black' data-row='2' data-col='7' onClick={handleMove.bind(this)}></div>
                 </div>
                 <div className='row'>
-                    <div className='col black' id='a5'>5</div>
-                    <div className='col white' id='b5'></div>
-                    <div className='col black' id='c5'></div>
-                    <div className='col white' id='d5'></div>
-                    <div className='col black' id='e5'></div>
-                    <div className='col white' id='f5'></div>
-                    <div className='col black' id='g5'></div>
-                    <div className='col white' id='h5'></div>
+                    <div className='col black' data-row='3' data-col='0' onClick={handleMove.bind(this)}>5</div>
+                    <div className='col white' data-row='3' data-col='1' onClick={handleMove.bind(this)}></div>
+                    <div className='col black' data-row='3' data-col='2' onClick={handleMove.bind(this)}></div>
+                    <div className='col white' data-row='3' data-col='3' onClick={handleMove.bind(this)}></div>
+                    <div className='col black' data-row='3' data-col='4' onClick={handleMove.bind(this)}></div>
+                    <div className='col white' data-row='3' data-col='5' onClick={handleMove.bind(this)}></div>
+                    <div className='col black' data-row='3' data-col='6' onClick={handleMove.bind(this)}></div>
+                    <div className='col white' data-row='3' data-col='7' onClick={handleMove.bind(this)}></div>
                 </div>
                 <div className='row'>
-                    <div className='col white' id='a4'>4</div>
-                    <div className='col black' id='b4'></div>
-                    <div className='col white' id='c4'></div>
-                    <div className='col black' id='d4'></div>
-                    <div className='col white' id='e4'></div>
-                    <div className='col black' id='f4'></div>
-                    <div className='col white' id='g4'></div>
-                    <div className='col black' id='h4'></div>
+                    <div className='col white' data-row='4' data-col='0' onClick={handleMove.bind(this)}>4</div>
+                    <div className='col black' data-row='4' data-col='1' onClick={handleMove.bind(this)}></div>
+                    <div className='col white' data-row='4' data-col='2' onClick={handleMove.bind(this)}></div>
+                    <div className='col black' data-row='4' data-col='3' onClick={handleMove.bind(this)}></div>
+                    <div className='col white' data-row='4' data-col='4' onClick={handleMove.bind(this)}></div>
+                    <div className='col black' data-row='4' data-col='5' onClick={handleMove.bind(this)}></div>
+                    <div className='col white' data-row='4' data-col='6' onClick={handleMove.bind(this)}></div>
+                    <div className='col black' data-row='4' data-col='7' onClick={handleMove.bind(this)}></div>
                 </div>
                 <div className='row'>
-                    <div className='col black' id='a3'>3</div>
-                    <div className='col white' id='b3'></div>
-                    <div className='col black' id='c3'></div>
-                    <div className='col white' id='d3'></div>
-                    <div className='col black' id='e3'></div>
-                    <div className='col white' id='f3'></div>
-                    <div className='col black' id='g3'></div>
-                    <div className='col white' id='h3'></div>
+                    <div className='col black' data-row='5' data-col='0' onClick={handleMove.bind(this)}>3</div>
+                    <div className='col white' data-row='5' data-col='1' onClick={handleMove.bind(this)}></div>
+                    <div className='col black' data-row='5' data-col='2' onClick={handleMove.bind(this)}></div>
+                    <div className='col white' data-row='5' data-col='3' onClick={handleMove.bind(this)}></div>
+                    <div className='col black' data-row='5' data-col='4' onClick={handleMove.bind(this)}></div>
+                    <div className='col white' data-row='5' data-col='5' onClick={handleMove.bind(this)}></div>
+                    <div className='col black' data-row='5' data-col='6' onClick={handleMove.bind(this)}></div>
+                    <div className='col white' data-row='5' data-col='7' onClick={handleMove.bind(this)}></div>
                 </div>
                 <div className='row'>
-                    <div className='col white' id='a2'>2</div>
-                    <div className='col black' id='b2'></div>
-                    <div className='col white' id='c2'></div>
-                    <div className='col black' id='d2'><img src='../../images/pawn-white.svg'/></div>
-                    <div className='col white' id='e2'></div>
-                    <div className='col black' id='f2'></div>
-                    <div className='col white' id='g2'></div>
-                    <div className='col black' id='h2'></div>
+                    <div className='col white' data-row='6' data-col='0' onClick={handleMove.bind(this)}>2</div>
+                    <div className='col black' data-row='6' data-col='1' onClick={handleMove.bind(this)}></div>
+                    <div className='col white' data-row='6' data-col='2' onClick={handleMove.bind(this)}></div>
+                    <div className='col black' data-row='6' data-col='3' onClick={handleMove.bind(this)}><img
+                        src='../../images/pawn-white.svg'/></div>
+                    <div className='col white' data-row='6' data-col='4' onClick={handleMove.bind(this)}></div>
+                    <div className='col black' data-row='6' data-col='5' onClick={handleMove.bind(this)}></div>
+                    <div className='col white' data-row='6' data-col='6' onClick={handleMove.bind(this)}></div>
+                    <div className='col black' data-row='6' data-col='7' onClick={handleMove.bind(this)}></div>
                 </div>
                 <div className='row'>
-                    <div className='col black' id='a1'>
+                    <div className='col black' data-row='7' data-col='0' onClick={handleMove.bind(this)}>
                         <span>1</span>a
                     </div>
-                    <div className='col white' id='b1'><span/>b</div>
-                    <div className='col black' id='c1'><span/>c</div>
-                    <div className='col white' id='d1'><span/>d</div>
-                    <div className='col black' id='e1'><span/>e</div>
-                    <div className='col white' id='f1'><span/>f</div>
-                    <div className='col black' id='g1'><span/>g</div>
-                    <div className='col white' id='h1'><span/>h</div>
+                    <div className='col white' data-row='7' data-col='1' onClick={handleMove.bind(this)}><span/>b</div>
+                    <div className='col black' data-row='7' data-col='2' onClick={handleMove.bind(this)}><span/>c</div>
+                    <div className='col white' data-row='7' data-col='3' onClick={handleMove.bind(this)}><span/>d</div>
+                    <div className='col black' data-row='7' data-col='4' onClick={handleMove.bind(this)}><span/>e</div>
+                    <div className='col white' data-row='7' data-col='5' onClick={handleMove.bind(this)}><span/>f</div>
+                    <div className='col black' data-row='7' data-col='6' onClick={handleMove.bind(this)}><span/>g</div>
+                    <div className='col white' data-row='7' data-col='7' onClick={handleMove.bind(this)}><span/>h</div>
                 </div>
             </div>
-            <button onClick={handlePosition}>set position</button>
         </>
     )
 }
