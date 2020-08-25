c4a4c62b539c615e5cbbfc6d5adf683ce67716f4
JakubMizera <kubamiz53@gmail.com>
2020-08-25 13:13:53 +0200

    board v2 trying to color squares

--- app/js/elements/boardV2.js
+++ app/js/elements/boardV2.js
@@ -4,21 +4,23 @@ const BoardV2 = () => {
 
     const boardArray = [];
     const boardSize = 8;
+    let color = ""
+
+    const pawnBlack = <img src='../../images/pawn-black.svg'/>
 
     for (let i = 0; i < boardSize; i++) {
         boardArray[i] = [];
         for (let j = 0; j < boardSize; j++) {
-            boardArray[i][j] = j;
+            boardArray[i][j] = "";
         }
     }
-
 
     console.log(boardArray);
     return (
         <div className='board'>{
             boardArray.map((el, i) => (
                 <div className='row' key={i}>{el.map((el, i) => (
-                    <div className='col' key={i}>{el}</div>
+                    <div className={`col ${(i % 2 === 1) ? "white" : "black"}`} key={i}>{el}</div>
                 ))}</div>
             ))
         }</div>
