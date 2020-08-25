215fad48160b7082d00ff1021f09813ccd3a7af6
JakubMizera <kubamiz53@gmail.com>
2020-08-25 11:48:42 +0200

    board v2 generated by map function

--- app/js/elements/boardV2.js
+++ app/js/elements/boardV2.js
@@ -2,8 +2,27 @@ import React from 'react';
 
 const BoardV2 = () => {
 
+    const boardArray = [];
+    const boardSize = 8;
+
+    for (let i = 0; i < boardSize; i++) {
+        boardArray[i] = [];
+        for (let j = 0; j < boardSize; j++) {
+            boardArray[i][j] = j;
+        }
+    }
 
 
+    console.log(boardArray);
+    return (
+        <div className='board'>{
+            boardArray.map((el, i) => (
+                <div className='row' key={i}>{el.map((el, i) => (
+                    <div className='col' key={i}>{el}</div>
+                ))}</div>
+            ))
+        }</div>
+    )
 }
 
 export default BoardV2;
